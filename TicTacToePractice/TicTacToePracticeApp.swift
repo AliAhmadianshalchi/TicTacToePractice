95217cd574e9ad460b0e842a7bd3c0874a813840
Ali Ahmadian <40244903+AliAhmadianshalchi@users.noreply.github.com>
2026-01-08 10:00:15 -0500

    Initial Commit

---
+++ TicTacToePractice/TicTacToePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToePracticeApp.swift
+//  TicTacToePractice
+//
+//  Created by Ali Ahmadian on 8/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
